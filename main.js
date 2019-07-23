d17223713c08e3f4dfcb892041605e02d801064d
tranhuy <tranvanhuy0202@gmail.com>
2019-07-23 10:49:14 +0700

    add load resources

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+//load tài nguyên
+var picsnames = ["background1.png", "background2.png", "background3.png", "background4.png", "bullet.png", "enemy.png", "plane.png"];
+var musicNames = ["bulletSound.mp3", "bgSound.mp3", "silence.mp3"];
+var picCount = 0;
+var musicCount = 0;
+var musics = [];
+
+for (var i = 0; i < picsnames.length; i++) {
+    var img = new Image();
+    img.src = "images/" + picsnames[i];
+    img.onload = function() {
+        picCount++;
+        if (picCount == picsnames.length) {
+            loadMusic();
+        }
+    }
+}
+
+function loadMusic() {
+    for (var i = 0; i < musicNames.length; i++) {
+        var music = new Audio();
+        music.src = "sounds/" + musicNames[i];
+        musics.push(music);
+        music.onloadedmetadata = function() {
+            musicCount++;
+            if (musicCount == musicNames.length) {
+                loadingDiv.style.display = "none";
+                main();
+                musics[4].volume = 0.5;
+                musics[4].loop = true;
+                musics[4].play();
+            }
+        }
+    }
+}
+
 let background = new Background();
 let plane = new Plane(canvas.width / 2 - PLANE_WIDTH / 2, canvas.height - (PLANE_HEIGHT + PLANE_DISTANCE_DEFAULT), PLANE_WIDTH, PLANE_HEIGHT, PLANE_SPEED, false, false, false);
 let gameBoard = new GameBoard();
